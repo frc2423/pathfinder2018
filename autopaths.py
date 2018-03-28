09c9315fe9dfb5d580773e60c2ffef8aeb09d2e6
Dwight Reed <reeddwight1@gmail.com>
2018-03-27 20:20:14 -0400

    fixed autopaths.py

--- autopaths.py
+++ autopaths.py
@@ -1,27 +1,15 @@
-import math
-import wpilib
 import pathfinder as pf
 
 
-# Robot attributes
-WHEEL_DIAMETER = 0.5  # 6 inches
-ENCODER_COUNTS_PER_REV = 360
-
-# Pathfinder constants
-MAX_VELOCITY = 5  # ft/s
-MAX_ACCELERATION = 6
-
-
-def get_trajectory(points, period=.02):
+def get_trajectory(points, period=.02, max_velocity=5, max_acceleration=6, max_jerk=120, wheelbase_width=2):
     # Set up the trajectory
     info, trajectory = pf.generate(points, pf.FIT_HERMITE_CUBIC, pf.SAMPLES_HIGH,
                                    dt=period,
-                                   max_velocity=MAX_VELOCITY,
-                                   max_acceleration=MAX_ACCELERATION,
-                                   max_jerk=120.0)
+                                   max_velocity=max_velocity,
+                                   max_acceleration=max_acceleration,
+                                   max_jerk=max_jerk)
 
-    # Wheelbase Width = 2 ft
-    modifier = pf.modifiers.TankModifier(trajectory).modify(2.0)
+    modifier = pf.modifiers.TankModifier(trajectory).modify(wheelbase_width)
     left = modifier.getLeftTrajectory()
     right = modifier.getRightTrajectory()
     return left, modifier.source, right
